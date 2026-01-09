e6e1c822b173c1258dba613089cf8cbd73385eac
kfoq7 <frbrizoi@gmail.com>
2026-01-08 22:41:39 -0500

    feat(config): updated hyprland config and fish

--- .config/fish/conf.d/fnm.fish
+++ .config/fish/conf.d/fnm.fish
@@ -4,3 +4,10 @@ if [ -d "$FNM_PATH" ]
   set PATH "$FNM_PATH" $PATH
   fnm env | source
 end
+
+# fnm
+set FNM_PATH "/home/kfoq7/.local/share/fnm"
+if [ -d "$FNM_PATH" ]
+  set PATH "$FNM_PATH" $PATH
+  fnm env | source
+end
